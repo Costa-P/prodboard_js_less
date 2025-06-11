94d1d7ce6c33408b08c15d17330f7ebb6eb15158
Costa-P <prk557@gmail.com>
2025-06-11 14:38:11 +0300

    save less_1

---
+++ less_1/hw_1.js
@@ -0,0 +1,40 @@
+  // task 1
+let title = 'Roman';
+let age = 47;
+let sex;
+let newVar;
+
+newVar = 100;
+console.log(newVar)
+
+console.log('Task 1');
+console.log(`title: ${title}`);
+console.log(`Age: ${age}`);
+document.body.append('Task 1');
+
+  // task 2
+const strUserData = 'User Data:';
+console.log('Task 2');
+console.log(strUserData + ' ' + title + ', ' + age + '.');
+
+    // task 3
+let m = prompt('age');
+
+console.log('Task 3');
+console.log(`User Age: ${age}; *2 = ${m*=2}; +12 = ${m+=12}; /2 = ${m/=2}; -'User Age' = ${m-=age}`);
+console.log(`Always "${m}"`);
+
+  // task 4
+let strUserAge = '47';
+console.log('Task 4');
+console.log(Number(strUserAge));
+console.log(Number(`User Age: ${strUserAge}`));
+  // task 5
+const UserDataControl = {
+  title: Boolean(title),
+  age: Boolean(age),
+  sex: Boolean(sex)
+}
+console.log('Task 4');
+console.log(UserDataControl);
+
